a5b30e936ebce717c41b253e515684df7a88db2c
Jamie Yu <jamiekyu@gmail.com>
2025-09-30 14:02:45 -0700

    Remove export from index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as Header } from './Header';
 export type { HeaderProps, NavigationItem } from './Header';
 export { getHeaderConfig, type HeaderConfig } from './config';
-export { initializeGoogleAnalytics, trackPageView, trackEvent, trackNavigationClick, type GoogleAnalyticsConfig } from './analytics';
